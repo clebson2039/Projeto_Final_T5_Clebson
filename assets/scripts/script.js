e57c72e70a4df94cf07211cf31afafc9fc56277d
clebson2039 <clebsonfarias1@gmail.com>
2023-02-08 01:04:56 -0300

    ajustes de css e html

---
+++ assets/scripts/script.js
@@ -0,0 +1,15 @@
+const html = document.querySelector("html");
+const button = document.querySelector("input[name=theme]");
+
+const intialColors = {
+    
+}
+
+const changeColors = (colors) => {
+
+}
+
+button = document.addEventListener("change", ({target}) =>{
+
+    target.checked ? changeColors(): changeColors()
+})
